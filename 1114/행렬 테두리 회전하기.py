1b6fdec3076a78e13e94e870dfd6cb9d85512e6f
rkdud007 <gayeongparkk@gmail.com>
2021-11-14 21:59:34 +0900

    1114

--- 1114/행렬 테두리 회전하기.py
+++ 1114/행렬 테두리 회전하기.py
@@ -1,25 +1,22 @@
 def solution(rows, columns, queries):
     matrix = [ [i+k * columns for i in range(1, columns+1)] for k in range(rows) ]
     answer = []
-    for x1,y1,x2,y2 in queries:
+    for a in queries:
+        [x1,y1,x2,y2] = a 
         compare = matrix[x1-1][y1-1]
         minValue = compare
-
         for k in range(x1-1,x2-1):
             i = matrix[k+1][y1-1]
             matrix[k][y1-1] = i
             minValue = min(minValue, i)
-
         for k in range(y1-1,y2-1):
             i = matrix[x2-1][k+1]
             matrix[x2-1][k] = i
             minValue = min(minValue, i)
-
         for k in range(x2-1,x1-1,-1):
             i = matrix[k-1][y2-1]
             matrix[k][y2-1] = i
             minValue = min(minValue, i)
-
         for k in range(y2-1,y1-1,-1):
             i = matrix[x1-1][k-1]
             matrix[x1-1][k] = i
